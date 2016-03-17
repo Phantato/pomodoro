22788d5effaff89b60e0c7ee0f409d5194d68802
ZHTan <tzh_1997@qq.com>
2016-03-17 12:39:08 +0800

    add some features add a counter which can stop the pomodoro automaticlly add a user recording system which now can basicly record total pomodoro passed

--- pomodoro.py
+++ pomodoro.py
@@ -1,22 +1,44 @@
+debug = True
+#debug = False
+
 import time
 import os
 currentDirectory = os.getcwd()
 wavDirectory = currentDirectory + "/WAV/"
+userDirectory = currentDirectory + "/User/"
 endwav = wavDirectory + "time.wav"
 
 def alart(times): os.system(("mplayer " + endwav +  "< /dev/null > /dev/null 2>1;") * times)
 
+with open(userDirectory + "user.bin", "br") as userFile:
+    userPomodoroTimes = int.from_bytes(userFile.read(4), "big")
+print(userPomodoroTimes)
+
+setPomodoroTimes = int(input("How many pomodoro would you start?:"))
+passedPomodoroTimes = 0
+print("Starting Pomodoro!")
+
+
 startTime = int(time.time())
-flag = True
-while 1:
+print("Start at " + time.strftime("%H:%M:%S"))
+restFlag = True
+print("")
+
+while setPomodoroTimes > passedPomodoroTimes:
     currentTime = int(time.time())
-    if flag and (currentTime - startTime) >= 1500:
+    if restFlag and ((currentTime - startTime) >= 1500 or (debug and currentTime - startTime >= 10)):
         alart(3)
         print("It's time to take a break!")
-        flag = False
+        restFlag = False
         startTime = int(time.time())
-    elif not flag and (currentTime - startTime) >= 300:
+        print("Start at " + time.strftime("%H:%M:%S"))
+        passedPomodoroTimes += 1
+        userPomodoroTimes += 1
+    elif not restFlag and ((currentTime - startTime) >= 300 or (debug and currentTime - startTime >= 3)):
         alart(3)
         print("It's time to work!")
-        flag = True
+        restFlag = True
         startTime = int(time.time())
+        print("Start at " + time.strftime("%H:%M:%S"))
+with open(userDirectory + "user.bin", "bw") as userFile:
+    userFile.write(userPomodoroTimes.to_bytes(4, "big"))
